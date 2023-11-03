cac6b9e15bc5f8d33b2ed9df673c1f513ecd1169
Paulo Tomé Gomes de Vargas <paulotomegomesdevargas@gmail.com>
2023-11-03 10:21:10 -0300

    Criando modal

--- src/components/CombatModal.tsx
+++ src/components/CombatModal.tsx
@@ -1,4 +1,107 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from '@mui/material/Modal';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import HeroCard from './HeroCard';
+import { useHero } from '@/context/HeroContext';
+import HeroList from './HeroList';
+/* const customStyles: React.CSSProperties = {
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+}; */
+
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+function CombatModal() {
+
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const { heroes, setHeroes } = useHero();
+
+  const filteredHeroes = heroes.filter((hero: { name: string; }) =>
+    hero.name.toLowerCase().search(/Abin/i) !== -1
+  );
+
+  return(
+    <div>
+       <Button onClick={handleOpen}>Open modal</Button>
+       <div className='card'>
+          {/* <HeroCard hero={undefined} startCombat={undefined} /> */}
+        </div>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+          <HeroList heroes={filteredHeroes} startCombat={undefined} />
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Text in a modal
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            </Typography>
+          </Box>
+        </Modal>
+    </div>
+  )
+  
+ 
+
+  /* 
+  const [selectedHeroes, setSelectedHeroes] = useState<string[]>([]);
+  const [isModalOpen, setModalOpen] = useState(true);
+
+  const handleHeroSelect = (hero: string) => {
+    setSelectedHeroes([...selectedHeroes, hero]);
+    if (selectedHeroes.length === 1) {
+      setModalOpen(true);
+    }
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+  return (
+    <div>
+      {/* Renderize os heróis e permita que o usuário selecione dois deles */}
+      {/* <Button onClick={() => handleHeroSelect("Hero 1")}>Selecionar Herói 1</Button>
+      <Button onClick={() => handleHeroSelect("Hero 2")}>Selecionar Herói 2</Button> */}
+      {/* Renderize outros botões para selecionar heróis conforme necessário */}
+      
+      {/* <Modal open={isModalOpen} onClose={closeModal} style={customStyles}>
+        <div>
+          <h2>Heróis Selecionados</h2>
+          {selectedHeroes.map((hero, index) => (
+            <div key={index}>
+              <h3>{hero}</h3>
+            </div>
+          ))}
+        </div>
+      </Modal>
+    </div>
+  ); }
+ }*/}
+
+export default CombatModal;
+
+
+/* import React from 'react';
 import { Modal, Box, Button, Typography } from '@mui/material';
 
 const CombatModal = ({ winner, onClose }) => {
@@ -20,3 +123,4 @@ const CombatModal = ({ winner, onClose }) => {
 };
 
 export default CombatModal;
+ */
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,40 @@
-import { useEffect, useState } from 'react';
-import HeroList from '../components/HeroList';
-import CombatModal from '../components/CombatModal';
-import { useHero } from '../context/HeroContext';
-import axios from 'axios';
+import { useEffect, useState } from "react";
+import HeroList from "../components/HeroList";
+import CombatModal from "../components/CombatModal";
+import { useHero } from "../context/HeroContext";
+import axios from "axios";
+
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 export default function Home() {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   const { heroes, setHeroes } = useHero();
-  const [filter, setFilter] = useState('');
+  const [filter, setFilter] = useState("");
   const [combatants, setCombatants] = useState([]);
   const [winner, setWinner] = useState(null);
 
   useEffect(() => {
-    axios.get('http://homologacao3.azapfy.com.br/api/ps/metahumans')
-      .then((response) => setHeroes(response.data, console.log("API : ", response.data)), );
+    axios
+      .get("http://homologacao3.azapfy.com.br/api/ps/metahumans")
+      .then((response) =>
+        setHeroes(response.data, console.log("API : ", response.data))
+      );
   }, []);
 
-  const filteredHeroes = heroes.filter(hero =>
+  const filteredHeroes = heroes.filter((hero) =>
     hero.name.toLowerCase().includes(filter.toLowerCase())
   );
 
@@ -25,29 +44,32 @@ export default function Home() {
   };
 
   return (
-    <div className=''>
-      <h1 style={{ color: 'white', textAlign: 'center', fontSize: '100px' }}>Hero Battle</h1>
-      <div className='search-column'>
-      <div>
-        <input
-        style={{ alignContent: 'center', fontSize: '50px' }}
-        type="text"
-        placeholder="Filtrar heróis por nome"
-        value={filter}
-        onChange={(e) => setFilter(e.target.value)}
-      />
-      </div>        
+    <div className="">
+      <h1 style={{ color: "white", textAlign: "center", fontSize: "100px" }}>
+        Hero Battle
+      </h1>
+      <div className="search-column">
+        <div>
+          <input
+            style={{ alignContent: "center", fontSize: "50px" }}
+            type="text"
+            placeholder="Filtrar heróis por nome"
+            value={filter}
+            onChange={(e) => setFilter(e.target.value)}
+          />
+          <div className="combat-modal">
+            <CombatModal /* winner={winner} onClose={() => setWinner(null)} */ />
+          </div>
+        </div>
+        
       </div>
-    <div className='container'>      
-      <div className='hero-container'>
-        <div className='hero-list'>
-          <HeroList heroes={filteredHeroes} startCombat={startCombat}/>
+      <div className="container">
+        <div className="hero-container">
+          <div className="hero-list">
+            <HeroList heroes={filteredHeroes} startCombat={startCombat} />
+          </div>
         </div>
       </div>
-      {winner && <CombatModal winner={winner} onClose={() => setWinner(null)} />}
-      
-    </div>
-   
     </div>
   );
 }
